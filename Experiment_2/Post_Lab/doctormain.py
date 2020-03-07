251ab24c4df65254a0c7af134b93cb7a3edc4077
Emmanigold <61401418+Emmanigold@users.noreply.github.com>
2020-03-07 23:12:05 +0800

    Add files via upload

---
+++ Experiment_2/Post_Lab/doctormain.py
@@ -0,0 +1,13 @@
+from doctor import Doctor
+def main():
+    d = Doctor()
+    print(d.greeting())
+    while True:
+        sentence = input("\n>> ")
+        if sentence.upper() == "QUIT":
+            print(d.farewell())
+            break
+        print(d.reply(sentence))
+
+if __name__ == "__main__":
+    main()
